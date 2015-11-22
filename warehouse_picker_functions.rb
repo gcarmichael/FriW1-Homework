3b2e7893da65ba4bdf98c917885ded53f0349e47
Gary Carmichael <g.carmichael90@gmail.com>
2015-11-22 18:04:19 +0000

    Advanced 1 complete.

--- warehouse_picker_functions.rb
+++ warehouse_picker_functions.rb
@@ -3,123 +3,153 @@ require 'pry-byebug'
 WAREHOUSE = [
   {
     bay: "a10",
-    item: "rubber band"
+    item: "rubber band",
+    distance: 1
   },
   {
     bay: "a9",
-    item: "glow stick"
+    item: "glow stick",
+    distance: 2
   },
   {
     bay: "a8",
-    item: "model car"
+    item: "model car",
+    distance: 3
   },
   {
     bay: "a7",
-    item: "bookmark"
+    item: "bookmark",
+    distance: 4
   },
   {
     bay: "a6",
-    item: "shovel"
+    item: "shovel",
+    distance: 5
   },
   {
     bay: "a5",
-    item: "rubber duck"
+    item: "rubber duck",
+    distance: 6
   },
   {
     bay: "a4",
-    item: "hanger"
+    item: "hanger",
+    distance: 7
   },
   {
     bay: "a3",
-    item: "blouse"
+    item: "blouse",
+    distance: 8
   },
   {
     bay: "a2",
-    item: "stop sign"
+    item: "stop sign",
+    distance: 9
   },
   {
     bay: "a1",
-    item: "needle"
+    item: "needle",
+    distance: 10
   },
   {
     bay: "c1",
-    item: "rusty nail"
+    item: "rusty nail",
+    distance: 11
   },
   {
     bay: "c2",
-    item: "drill press"
+    item: "drill press",
+    distance: 12
   },
   {
     bay: "c3",
-    item: "chalk"
+    item: "chalk",
+    distance: 13
   },
   {
     bay: "c4",
-    item: "word search"
+    item: "word search",
+    distance: 14
   },
   {
     bay: "c5",
-    item: "thermometer"
+    item: "thermometer",
+    distance: 15
   },
   {
     bay: "c6",
-    item: "face wash"
+    item: "face wash",
+    distance: 16
   },
   {
     bay: "c7",
-    item: "paint brush"
+    item: "paint brush",
+    distance: 17
   },
   {
     bay: "c8",
-    item: "candy wrapper"
+    item: "candy wrapper",
+    distance: 18
   },
   {
     bay: "c9",
-    item: "shoe lace"
+    item: "shoe lace",
+    distance: 19
   },
   {
     bay: "c10",
-    item: "leg warmers"
+    item: "leg warmers",
+    distance: 20
   },
   {
     bay: "b1",
-    item: "tyre swing"
+    item: "tyre swing",
+    distance: 21
   },
   {
     bay: "b2",
-    item: "sharpie"
+    item: "sharpie",
+    distance: 22
   },
   {
     bay: "b3",
-    item: "picture frame"
+    item: "picture frame",
+    distance: 23
   },    
   {
     bay: "b4",
-    item: "photo album"
+    item: "photo album",
+    distance: 24
   },  
   {
     bay: "b5",
-    item: "nail filer"
+    item: "nail filer",
+    distance: 25
   },
   {
     bay: "b6",
-    item: "tooth paste"
+    item: "tooth paste",
+    distance: 26
   },
   {
     bay: "b7",
-    item: "bath fizzers"
+    item: "bath fizzers",
+    distance: 27
   },
   {
     bay: "b8",
-    item: "tissue box"
+    item: "tissue box",
+    distance: 28
   },
   {
     bay: "b9",
-    item: "deodorant"
+    item: "deodorant",
+    distance: 29
   },
   {
     bay: "b10",
-    item: "cookie jar"
+    item: "cookie jar",
+    distance: 30
 }
 ]
 
@@ -137,8 +167,7 @@ end
 def items_at_bays(*bay) #(bay1, bay2, bay3)
   # items = WAREHOUSE.select{ |item| item[:bay] == bay1 || item[:bay] == bay2 || item[:bay] == bay3 }
   # items.map{|item| item[:item]}.join(", ")
-  bay.map!{|item| item = item_at_bay(item)}
-  .join(", ")
+  bay.map!{|item| item = item_at_bay(item)}.join(", ")
   # bay.join(", ")
 end
 
@@ -146,9 +175,23 @@ def bays_for_items(*item)
   item.map!{|bay| bay = bay_for_item(bay)}.join(", ")
 end
 
+# def furthest_distance(*bay)
+#   items_at_bays(bay)
+# end
 
+def distance(bay1, bay2, bay3)
+  location = WAREHOUSE.select{|item| item[:bay] == bay1 || item[:bay] == bay2 || item[:bay] == bay3}
+    max = location.max_by{|item| item[:distance]}
+    min = location.min_by{|item| item[:distance]}
+    max[:distance]-min[:distance]
 
+  # location_max = WAREHOUSE.select{|item| item[:bay] == bay1 || item[:bay] == bay2 || item[:bay] == bay3}
+  #                         .max_by{|item| item[:distance]}
+  # location_min = WAREHOUSE.select{|item| item[:bay] == bay1 || item[:bay] == bay2 || item[:bay] == bay3}
+  #                         .min_by{|item| item[:distance]}
+  # puts location_max
+  # puts location_min
+  # location_max[:distance] - location_min[:distance]
 
-
-
+end
 
--- warehouse_picker_runner.rb
+++ warehouse_picker_runner.rb
@@ -26,10 +26,11 @@ puts "\n"
 # Given a list of bays, list the items in those bays, and calculate the distance from the two furthest apart bays.
 
 # given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
-
-
-
-
+# furhtest_distance = furthest_distance('b5', 'b10', 'b6')
+found_items = items_at_bays('b5', 'b10', 'b6')
+max_distance = distance('b5', 'b10', 'b6')
+puts "the items in bays b5, b10, and b6 are #{found_items}, and the distance between them is #{max_distance}; expected nail filer, cookie jar, and tooth paste, with distance 5"
+puts "\n"
 
 
 
